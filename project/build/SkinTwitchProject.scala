f7a3bbbc56f08b246ccb705284ecf0182f03cec1
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-25 15:31:51 -0400

    Pull in scalasignal and mocaputils via ivy, and not using jar files.

--- project/build/SkinTwitchProject.scala
+++ project/build/SkinTwitchProject.scala
@@ -11,6 +11,26 @@ with Eclipsify {
   val scalaTest = "org.scalatest" %% "scalatest" % "1.4.1"
   // val scalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.8"
 
+  // ScalaLA stuff
+  val scalaNLPRepo = "ScalaNLP" at "http://repo.scalanlp.org/repo"
+  val ondexRepo = "ondex" at 
+    "http://ondex.rothamsted.bbsrc.ac.uk/nexus/content/groups/public"
+  val scalala = "org.scalala" %%  "scalala" % "1.0.0.RC2-SNAPSHOT"
+
+  // Apache Commons Math
+  val commonsMath = "org.apache.commons" % "commons-math" % "2.0"  
+  
+  // JFreeChart
+  val JCommon = "jfree" % "jcommon" % "1.0.16"
+  val JFreeChart = "jfree" % "jfreechart" % "1.0.13"
+  val XMLGraphicsCommons = "org.apache.xmlgraphics" % "xmlgraphics-commons" % 
+      "1.3.1"
+  val IText = "com.lowagie" % "itext" % "2.1.5" intransitive()  
+    
+  // scalasignal and mocaputils
+  val scalaSignal = "GitHub" %% "scalasignal" % "0.3-SNAPSHOT"
+  val mocapUtils = "MEPC" %% "motion-capture-utilities" % "0.2-SNAPSHOT"
+  
   // enable unchecked warnings
   //override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
