0a8b0b51b548fe683db61dd12e53607c076a205a
Jonathan Merritt <merritt@unimelb.edu.au>
2011-06-13 09:09:12 -0400

    dgtri method for deformation gradient tensor estimation.

---
+++ src/test/scala/skintwitch/TensorUtilsTest.scala
@@ -0,0 +1,12 @@
+package skintwitch
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+class TensorUtilsTest extends FunSuite with ShouldMatchers {
+
+  test("principalComp") (pending)
+  
+  test("dgtri") (pending)
+  
+}
