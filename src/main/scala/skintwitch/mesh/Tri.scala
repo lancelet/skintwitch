200effdf637e10ea7768a27918825ee854d60631
Jonathan Merritt <merritt@unimelb.edu.au>
2011-07-05 15:05:37 -0400

    Triangular facet: barycentric coordinates and distance-to-point.

---
+++ src/main/scala/skintwitch/mesh/Tri.scala
@@ -0,0 +1,116 @@
+package skintwitch.mesh
+
+import skintwitch.rman.V3
+
+/** Triangular facet. 
+ * 
+ *  The outward-facing normal of the triangle is defined by the right-handed
+ *  ordering of the vertices: a, b, c. */
+trait Tri {
+  import Tri._
+  
+  /** Vertex of the triangle. */
+  val a: (Double, Double, Double)
+  /** Vertex of the triangle. */
+  val b: (Double, Double, Double)
+  /** Vertex of the triangle. */
+  val c: (Double, Double, Double)
+
+  private lazy val abxac: V3 = (b - a) x (c - a)
+  private lazy val n: V3 = abxac.normalized
+  
+  /** Normal of the triangle. */
+  lazy val normal: (Double, Double, Double) = (n.e0, n.e1, n.e2)
+  
+  /** Projects a point into the plane defined by the triangle.
+   * 
+   *  @param p point to project
+   *  @return projected point */
+  def projectInto(p: (Double, Double, Double)): (Double, Double, Double) = {
+    val ap = p - a
+    val pp = a + ap - n * (ap dot n)
+    (pp.e0, pp.e1, pp.e2)
+  }
+  
+  /** Projects a point into the plane defined by the triangle and computes
+   *  its barycentric coordinates.
+   *  
+   *  The barycentric coordinates are ordered so that they reference the
+   *  (a, b, c) vertices respectively.
+   *  
+   *  @param p point to project
+   *  @param pAlreadyProjected true if `p` has already been projected into
+   *    the plane of the triangle
+   *  @return barycentric coordinates of `p` */
+  def projectIntoBarycentric(
+      p: (Double, Double, Double),
+      pAlreadyProjected: Boolean = false
+  ): 
+    (Double, Double, Double) = 
+  {
+    val pp = implicitly[V3](if (pAlreadyProjected) p else projectInto(p))
+    val a1 = (((c - b) x (pp - b)) dot n) * 0.5
+    val a2 = (((a - c) x (pp - c)) dot n) * 0.5
+    val a3 = (((b - a) x (pp - a)) dot n) * 0.5
+    val aTot = a1 + a2 + a3
+    val l1 = a1 / aTot
+    val l2 = a2 / aTot
+    val l3 = a3 / aTot
+    (l1, l2, l3)
+  }
+  
+  /** Computes the shortest distance between a point and the triangle.
+   *  
+   *  @param p point for which to find the shortest distance
+   *  @return shortest distance between the triangle and `p` */
+  def distanceTo(p: (Double, Double, Double)): Double = {
+    val pp = projectInto(p)
+    val bary = projectIntoBarycentric(p, true)
+    if (bary._1 >= 0.0 && bary._1 <= 1.0 &&
+        bary._2 >= 0.0 && bary._2 <= 1.0 &&
+        bary._3 >= 0.0 && bary._3 <= 1.0) { // inside the triangle
+      (pp - p).length
+    } else if (bary._2 < 0 && bary._3 < 0) { // definitely closest to vertex a
+      (a - p).length
+    } else if (bary._1 < 0 && bary._3 < 0) { // definitely closest to vertex b
+      (b - p).length
+    } else if (bary._1 < 0 && bary._2 < 0) { // definitely closest to vertex c
+      (c - p).length
+    } else {
+      // here, we may be closest to either an edge or a vertex, so find
+      //  the closest edge and use the distanceToEdge() method
+      val (ea, eb) = if (bary._3 < 0) {
+        (a, b)
+      } else if (bary._2 < 0) {
+        (a, c)
+      } else {
+        assert(bary._1 < 0)
+        (c, b)
+      }
+      distanceToEdge(ea, eb, p)
+    }
+  }
+  
+  /** Finds the shortest distance between a point and an edge.
+   * 
+   *  @param ea edge vertex a
+   *  @param eb edge vertex b
+   *  @param p point for which to find the distance
+   *  @return shortest distance between `ea -> eb` and `p` */
+  private def distanceToEdge(ea: (Double, Double, Double),
+                             eb: (Double, Double, Double),
+                             p: (Double, Double, Double)): Double = 
+  {
+    val eaeb = eb - ea
+    val q = (eaeb dot (p - ea)) / eaeb.length2
+    val qnorm = if (q < 0) 0 else if (q > 1) 1 else q
+    val linePt = ea + eaeb * qnorm
+    (linePt - p).length
+  }
+  
+}
+
+object Tri {
+  implicit def tupleToV3(x: (Double, Double, Double)): V3 = 
+    V3(x._1, x._2, x._3)
+}
---
+++ src/test/scala/skintwitch/mesh/TriTest.scala
@@ -0,0 +1,64 @@
+package skintwitch.mesh
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+class TriTest extends FunSuite with ShouldMatchers {
+
+  test("normal") {
+    val tri = new Tri {
+      val a = (0.0, 0.0, 0.0)
+      val b = (0.0, 0.0, 1.0)
+      val c = (1.0, 0.0, 0.0)
+    }
+    val n = tri.normal
+    n._1 should be (0.0 plusOrMinus 1e-10)
+    n._2 should be (1.0 plusOrMinus 1e-10)
+    n._3 should be (0.0 plusOrMinus 1e-10)
+  }
+  
+  test("projectInto") {
+    val tri = new Tri {
+      val a = (0.0, 0.0, 0.0)
+      val b = (0.0, 0.0, 1.0)
+      val c = (1.0, 0.0, 0.0)      
+    }
+    val p = tri.projectInto((0.5, 50.0, 0.3))
+    p._1 should be (0.5 plusOrMinus 1e-10)
+    p._2 should be (0.0 plusOrMinus 1e-10)
+    p._3 should be (0.3 plusOrMinus 1e-10)
+  }
+
+  test("projectIntoBarycentric") {
+    val tri = new Tri {
+      val a = (0.0, 0.0, 0.0)
+      val b = (0.0, 0.0, 1.0)
+      val c = (1.0, 0.0, 0.0)      
+    }
+    val b = tri.projectIntoBarycentric((0.0, 5.0, 0.0))
+    b._1 should be (1.0 plusOrMinus 1e-10)
+    b._2 should be (0.0 plusOrMinus 1e-10)
+    b._3 should be (0.0 plusOrMinus 1e-10)
+    val c = tri.projectIntoBarycentric((0.5, 5.0, 0.5))
+    c._1 should be (0.0 plusOrMinus 1e-10)
+    c._2 should be (0.5 plusOrMinus 1e-10)
+    c._3 should be (0.5 plusOrMinus 1e-10)
+    val d = tri.projectIntoBarycentric((1.0, -10.0, 1.0))
+    d._1 should be (-1.0 plusOrMinus 1e-10)
+    d._2 should be (1.0 plusOrMinus 1e-10)
+    d._3 should be (1.0 plusOrMinus 1e-10)
+  }
+  
+  test("distanceTo") {
+    val tri = new Tri {
+      val a = (-1.0, -1.0, 0.0)
+      val b = ( 1.0, -1.0, 0.0)
+      val c = (-1.0,  1.0, 0.0)
+    }
+    tri.distanceTo((-0.453, -0.472, 1.0)) should be (1.0 plusOrMinus 1e-10)
+    tri.distanceTo((0.0, -2.0, 1.0)) should be (1.41 plusOrMinus 1e-2)
+    tri.distanceTo((1.707, -1.707, 1.0)) should be (1.41 plusOrMinus 1e-2)
+    tri.distanceTo((1.892, -1.452, 1.0)) should be (1.41 plusOrMinus 1e-2)
+  }
+  
+}
