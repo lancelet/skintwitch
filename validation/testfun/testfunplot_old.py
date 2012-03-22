47c6df6ddf5191496822997f9131364318ec9f16
Jonathan Merritt <merritt@unimelb.edu.au>
2012-03-22 15:21:50 -0400

    Old version of testfunplot (for record purposes).

--- validation/testfun/testfunplot_old.py
+++ validation/testfun/testfunplot_old.py
@@ -16,7 +16,9 @@ We use matplotlib for plotting.
 """
 
 import numpy
-from numpy import array, cos, sin
+from numpy import array, asarray, cos, dot, sin
+import numpy.linalg
+from numpy.linalg import eig
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as pyplot
 
@@ -53,5 +55,47 @@ h = omega * omega * k
 dgxx = -h * sin(omega * X + phi_x) * sin(omega * Y + phi_y) + 1
 dgxy =  h * cos(omega * X + phi_x) * cos(omega * Y + phi_y)
 # F = [ dgxx dgxy; dgxy dgxx ]
+# flatten out the components of the grid, so that we can process them
+#  sequentially more easily
+dgxxxy = zip(dgxx.flatten(), dgxy.flatten())
+# function to produce tensor principal vectors
+def xxxy_to_prinvecs(xxxy):
+    (xx, xy) = xxxy
+    F = array([[xx, xy], [xy, xx]])
+    #C = dot(F.transpose(), F) # right Cauchy-Green tensor
+    (el, ev) = eig(F)
+    #print(eig(F))
+    #print(F)
+    #print(ev)
+    (dx1, dy1) = (el[0] * ev[0,0], el[0] * ev[1,0])
+    (dx2, dy2) = (el[1] * ev[0,1], el[1] * ev[1,1])
+    #print(dx1, dy1)
+    #print(dx2, dy2)
+    #print("---")
+    return [dx1, dy1, dx2, dy2]
+prinvecs = asarray(map(xxxy_to_prinvecs, dgxxxy)).transpose()
+(dx1, dy1, dx2, dy2) = (prinvecs[0,:], prinvecs[1,:], prinvecs[2,:], 
+    prinvecs[3,:])
+dx1 = dx1.reshape(X.shape)
+dy1 = dy1.reshape(X.shape)
+dx2 = dx2.reshape(X.shape)
+dy2 = dy2.reshape(X.shape)
+
+stride = 2 * stride
+pyplot.quiver(
+    X[::stride, ::stride],
+    Y[::stride, ::stride],
+    dx1[::stride, ::stride],
+    dy1[::stride, ::stride],
+    color = 'b',
+    width = 0.001)
+pyplot.quiver(
+    X[::stride, ::stride],
+    Y[::stride, ::stride],
+    dx2[::stride, ::stride],
+    dy2[::stride, ::stride],
+    color = 'g',
+    width = 0.001)
+
 
 pyplot.show()
