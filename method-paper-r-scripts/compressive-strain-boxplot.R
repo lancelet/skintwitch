57d8fbec540823aa0da8a6cbf212caef1b3b09cf
Jonathan Merritt <merritt@unimelb.edu.au>
2012-04-25 12:34:59 -0400

    Box plot of maximum compressive strains using R.

---
+++ method-paper-r-scripts/compressive-strain-boxplot.R
@@ -0,0 +1,48 @@
+library(tikzDevice)
+library(ggplot2)
+
+# Set working directory (hard coded)
+setwd("/Users/jsm/Documents/dev/skintwitch/method-paper-r-scripts")
+
+# Set TikZ options
+standAlone = FALSE
+fontSize = 12
+tf = file.path(getwd(), "compressive-strain-boxplot.tex")
+tikz(tf, standAlone = standAlone, width = 4, height = 4, pointsize=fontSize)
+options(tikzLatex = "xelatex")
+
+# Read the CSV file containing minimum principal (compressive) strains
+csvdata = read.csv("../method-paper-output/min-prin-strains.csv")
+
+# Drop girthline data
+csvdata = subset(csvdata, !csvdata$Trial == "Girthline")
+
+# ANOVA
+# This is a one-way ANOVA, with a Tukey HSD post-hoc test
+anv = aov(Strain ~ Trial, data = csvdata)
+print(summary(anv))
+print(TukeyHSD(anv))
+# The data does not look very Normal (most groups are quite skewed), so we'll
+#  also do a Kruskal-Wallis ANOVA and a Wilcoxon rank sum test
+kruskal.test(csvdata)
+pairwise.wilcox.test(csvdata$Strain, csvdata$Trial)
+
+# Box plot
+q <- qplot(csvdata$Trial, csvdata$Strain, data = csvdata, geom="boxplot",
+    xlab="Trial Location / Control",
+    ylab="Maximum Compressive Strain at $\\mu_*$\n(dimensionless)") + 
+    theme_bw(base_size=fontSize) +
+    ylim(-0.15001, 0.00) +
+    opts(axis.title.x = theme_text(vjust=-0.9, size=fontSize)) +
+    opts(axis.text.x = theme_text(angle=90, size=fontSize, hjust=1)) +
+    opts(axis.title.y = theme_text(angle=90, hjust=0.5, vjust=0.35, size=fontSize)) +
+    opts(panel.border = theme_rect(fill = NA, colour="black"))
+print(q)
+dev.off()
+
+# Show the box plot
+if (standAlone) {
+    tools::texi2dvi(tf, pdf=TRUE)
+    system(paste(getOption("pdfviewer"), 
+        file.path(getwd(), "compressive-strain-boxplot.pdf")))
+}
